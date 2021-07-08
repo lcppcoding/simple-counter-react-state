4b18438be028af29721044c2e332c407df33561e
Lucas Pessoa <lcppcoding@gmail.com>
2021-07-08 15:17:37 -0300

    uncommited changes before setState and Helper part

--- src/Counter.js
+++ src/Counter.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
 
+function increment() {
+  this.setState((state, props) => {
+    const { max, step } = props;
+    if (state.count >= max) return;
+    return { count: state.count + step }
+  },
+  () => {
+    console.log(this.state);
+  });
+}
 class Counter extends Component {
   constructor(props) {
     super(props);
@@ -10,13 +20,12 @@ class Counter extends Component {
     this.increment = this.increment.bind(this);
     this.decrement = this.decrement.bind(this);
     this.reset = this.reset.bind(this);
-
-
   }
 
   increment() {
-    this.setState({ count: this.state.count + 1 });
+    this.setState(increment);
   }
+
 
   decrement() {
     this.setState({ count: this.state.count - 1 });
